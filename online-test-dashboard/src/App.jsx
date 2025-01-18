69926317aaf04b6040ffe7d2ef17c6e125f67d03
saurabhkeyideas <saurabhkumar.singh@keyideasglobal.com>
2025-01-18 13:33:12 +0530

    sidebar completed

--- online-test-dashboard/src/App.jsx
+++ online-test-dashboard/src/App.jsx
@@ -22,3 +22,54 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+// // import { useState } from 'react'
+// import React from 'react';
+// import Header from './components/header';
+// import Sidebar from './components/sidebar';
+// // import LeftSection from './components/LeftSection';
+// // import RightSection from './components/RightSection';
+
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <Header />
+//         <Sidebar/>
+//     <div className="dashboard-container">
+//      <div className="left-section">
+//     <div className="tabs">
+//       <button className="tab active" data-tab="my-library">My Library</button>
+//       <button className="tab" data-tab="hosted-sessions">Hosted Sessions</button>
+//     </div>
+//     <div className="content my-library-content">
+//     </div>
+//     <div className="content hosted-sessions-content hidden">
+//     </div>
+//   </div>
+ 
+//   <div className="right-section">
+//     <div className="tabs">
+//       <button className="tab active" data-tab="mcq">MCQ's</button>
+//       <button className="tab" data-tab="leaderboard">Leaderboard</button>
+//     </div>
+//     <div className="content mcq-content">
+     
+//     </div>
+//     <div className="content leaderboard-content hidden">
+     
+//     </div>
+//   </div>
+// </div>
+
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
--- online-test-dashboard/src/components/sidebar.jsx
+++ online-test-dashboard/src/components/sidebar.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import '../styles/sidebar.css'; 
+import createIcon from '../assets/images/create.png';
+import battleIcon from '../assets/images/Battle-brains.png'; 
+import serviceIcon from '../assets/images/service.png'; 
+import UpgradeIcon from '../assets/images/Upgrade.png';
+import ReferIcon from '../assets/images/Refer-a-friend.png';
+
+
 
 const Sidebar = () => {
   return (
@@ -10,30 +17,88 @@ const Sidebar = () => {
       <div className="profile-role">Student</div>
       </div> 
       </div>
-      <button className="create-button">+ Create</button>
+      <button className="create-button"> 
+      <img src={createIcon} alt="Create" className="create-icon" />
+      </button>
       <nav className="nav-options">
         <ul>
-          <li><a href="#">My Library</a></li>
-          <li><a href="#">Smart Review</a></li>
-          <li><a href="#">Hosted Sessions</a></li>
-          <li><a href="#">Battle of the Brains</a></li>
-          <li><a href="#">Groups</a></li>
-          <li><a href="#">Marketplace</a></li>
-          <li><a href="#">Rewards</a></li>
-          <li><a href="#">Notification</a></li>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Settings</a></li>
-          <li><a href="#">How to Use</a></li>
-          <li><a href="#">Support Hub</a></li>
-          <li><a href="#">Logout</a></li>
+          <div className="first-li-div">
+          <li>
+          <span className='css_sprites sprite_library'></span> 
+          <a href="#">My Library</a>
+          </li>
+          <li>
+          <span className='css_sprites css_spritess sprite_smart'></span> 
+          <a href="#">Smart Review</a>
+          </li>
+          <li>
+          <span className='css_sprites sprite_hosted'></span> 
+          <a href="#">Hosted Sessions</a>
+          </li>
+          </div>
+          <li><a href="#" className='battle-a'>
+          <span className='css_sprites sprite_battle'></span> 
+          <img src={battleIcon} alt="battle" className="battle-icon" />
+          </a></li>
+          <div className="first-li-div">
+          <li>
+          <span className='css_sprites sprite_group'></span> 
+          <a href="#">Groups</a>
+          </li>
+          <li>
+          <span className='css_sprites sprite_marketplace'></span> 
+          <a href="#">Marketplace</a>
+          </li>
+          </div>
+
+          <div className="first-li-div">
+          <li>
+          <span className='css_sprites sprite_rewards'></span> 
+          <a href="#">Rewards</a>
+          </li>
+          <li>
+          <span className='css_sprites css_spritess sprite_notification'></span> 
+          <a href="#">Notification</a>
+          </li>
+          <li>
+          <span className='css_sprites sprite_profile'></span> 
+          <a href="#">Profile</a>
+          </li>
+          <li>
+          <span className='css_sprites sprite_setting'></span> 
+          <a href="#">Settings</a>
+          </li>
+          <li>
+          <span className='css_sprites sprite_use'></span> 
+          <a href="#">How to Use</a>
+          </li>
+          <li>
+          <span className='css_sprites css_spritess sprite_support'></span> 
+          <a href="#">Support Hub</a>
+          </li>
+          <li>
+          <span className='css_sprites sprite_logout'></span> 
+          <a href="#">Logout</a>
+          </li>
+          </div>
         </ul>
       </nav>
-      <div className="other-links">
-        <a href="#">At your service</a>
+      <div className="first-li-div other-links">
+       <span>Other Links</span>
+        <a href="#">
+        <span className='css_sprites sprite_service'></span> 
+        <img src={serviceIcon} alt="Create" className="serviceIcon" />
+        </a>
       </div>
       <div className="upgrade-section">
-        <button className="upgrade-button">Upgrade</button>
-        <button className="refer-button">Refer a friend & earn more Coins</button>
+        <button className="upgrade-button">
+        <span className='css_sprites sprite_upgrade'></span> 
+        <img src={UpgradeIcon} alt="Create" className="upgradeIcon" />
+        </button>
+        <button className="refer-button">
+        <span className='css_sprites sprite_refer'></span> 
+        <img src={ReferIcon} alt="Create" className="ReferIcon" />
+        </button>
       </div>
     </div>
   );
